c15dbb5f1444dd04f4e46c7467f52649d96e1a01
Tifa <tiphereth-a@qq.com>
2022-04-18 10:14:31 +0800

    feat: add Luogu P2675

---
+++ src/Luogu/2675/0_410523635.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+using u64 = uint64_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using pii = pair<int, int>;
+#define _for(i, l, r, vals...) for (decltype(l + r) i = (l), i##end = (r), ##vals; i <= i##end; ++i)
+#define _rfor(i, r, l, vals...) for (make_signed_t<decltype(r - l)> i = (r), i##end = (l), ##vals; i >= i##end; --i)
+#define _foreach_val(i, container) for (auto i : container)
+#define _foreach_ref(i, container) for (auto &i : container)
+#define _foreach_cref(i, container) for (const auto &i : container)
+#define _foreach_rref(i, container) for (auto &&i : container)
+#define _foreach_iter(it, container) for (auto it = (container).begin(); it != (container).end(); ++it)
+#define _foreach_iter_range(it, container, l, r) for (auto it = (container).begin() + l; it != (container).begin() + r; ++it)
+#define _ins(a) std::inserter((a), (a).begin())
+#define _all(a) (a).begin(), (a).end()
+#define _set_nul(a) memset(a, 0, sizeof(a))
+#define _set_inf(a) memset(a, 0x3f, sizeof(a))
+#define _set_nul_n(a, n) memset(a, 0, sizeof(*(a)) * (n))
+#define _set_inf_n(a, n) memset(a, 0x3f, sizeof(*(a)) * (n))
+#define _run_exit(expressions) _run_return(expressions, 0)
+#define _run_return(expressions, val) return (expressions), val
+#define _run_return_void(expressions) \
+    {                                 \
+        expressions;                  \
+        return;                       \
+    }
+#define _run_break(expressions) \
+    {                           \
+        expressions;            \
+        break;                  \
+    }
+#define _run_continue(expressions) \
+    {                              \
+        expressions;               \
+        continue;                  \
+    }
+#define _mid(l, r) ((l) + (((r) - (l)) >> 1))
+#define _len(l, r) ((r) - (l) + 1)
+#define _lowbit(x) (1 << __builtin_ctz(x))
+#define _lowbit_64(x) (1 << __builtin_ctzll(x))
+#define _debug                                              \
+    {                                                       \
+        fprintf(stderr, "%d %s\n", __LINE__, __FUNCTION__); \
+        fflush(stderr);                                     \
+    }
+template <class T>
+bool chkmin(T &a, T b) { return b < a ? a = b, true : false; }
+template <class T>
+bool chkmax(T &a, T b) { return a < b ? a = b, true : false; }
+const uint32_t OFFSET = 5;
+const uint32_t N = 10007, M = 1e6 + OFFSET;
+const uint32_t MOD = 10007;
+template <typename Tp = i64>
+inline constexpr Tp qpow(Tp a, Tp b = MOD - 2, const Tp &mod = MOD) {
+    Tp res(1);
+    for (; b; b >>= 1, (a *= a) %= mod)
+        if (b & 1) (res *= a) %= mod;
+    return res;
+};
+i64 fact[N * 2], inv_fact[N];
+auto __STATIC__ = []() {
+    static i64 ffact[N];
+    ffact[0] = fact[0] = inv_fact[0] = fact[1] = inv_fact[1] = 1;
+    _for(i, 2, N * 2 - 1) fact[i] = fact[i - 1] * i % MOD;
+    _for(i, 1, N - 1) ffact[i] = ffact[i - 1] * fact[i] % MOD;
+    i64 _ = qpow(ffact[N - 1]);
+    _rfor(i, N - 1, 2) {
+        inv_fact[i] = _ * ffact[i - 1] % MOD;
+        _ = _ * fact[i] % MOD;
+    }
+    return 0.0;
+}();
+auto m_choose_n = [](int m, int n, i64 mod = MOD) -> i64 { return m < n ? 0 : fact[m] * inv_fact[n] % mod * inv_fact[m - n] % mod; };
+// #define MULTI_CASES
+int a[M];
+inline auto solve() -> void {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i += 2) a[(i + 1) / 2] = i;
+    for (int i = 2; i <= n; i += 2) a[n - (i - 1) / 2] = i;
+    int ans = 0;
+    _for(i, 1, n) (ans += m_choose_n((n - 1) / MOD, (i - 1) / MOD) * m_choose_n((n - 1) % MOD, (i - 1) % MOD) % MOD * a[i] % MOD) %= MOD;
+    cout << ans;
+}
+int main() {
+#ifdef _LOCAL_
+    auto _CLOCK_ST = std::chrono::steady_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifdef MULTI_CASES
+    int _t;
+    cin >> _t;
+    while (_t--)
+#endif
+        solve();
+#ifdef _LOCAL_
+    std::clog << "\n---\n"
+              << "Time used: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - _CLOCK_ST).count() << " ms" << std::endl;
+#endif
+    return 0;
+}
