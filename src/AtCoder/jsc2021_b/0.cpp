7d1417f32c8e109bcea0181459b36eecf64ebe28
Tifa <tiphereth-a@qq.com>
2022-05-07 11:12:17 +0800

    feat: add AtCoder jsc2021_b

---
+++ src/AtCoder/jsc2021_b/0.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 5e3 + 5;
+const int MOD = 1e9 + 7;
+int main() {
+#ifndef ONLINE_JUDGE
+    clock_t _CLOCK_ST = clock();
+#endif
+    int m, n;
+    cin >> m >> n;
+    vector<int> a, ans;
+    for (int i = 1, _; i <= m + n; ++i) {
+        cin >> _;
+        a.push_back(_);
+    }
+    sort(a.begin(), a.end());
+    for (auto it = a.begin(); it != a.end(); ++it) {
+        if (it == a.end() - 1) {
+            ans.push_back(*it);
+            continue;
+        }
+        if (*it == *(it + 1)) {
+            ++it;
+            continue;
+        }
+        ans.push_back(*it);
+    }
+    for (auto it = ans.begin(); it != ans.end(); ++it) cout << *it << " \n"[it == ans.end() - 1];
+#ifndef ONLINE_JUDGE
+    std::cerr
+        << "\n---\n"
+        << "Time used: " << clock() - _CLOCK_ST << std::endl;
+#endif
+    return 0;
+}
