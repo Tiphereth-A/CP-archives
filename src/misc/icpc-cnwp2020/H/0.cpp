23670645a63ec4bccdddcb50cca37bcf099d7d44
Tifa <tiphereth-a@qq.com>
2022-05-07 23:53:04 +0800

    feat: update

---
+++ src/misc/icpc-cnwp2020/H/0.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using ll = long long;
+const int INF = 0x3f3f3f3f;
+const long long INFLL = 0x3f3f3f3f3f3f3f3fLL;
+int _w, _t;
+ll a, b, c;
+using namespace std;
+ll gcd(ll u, ll v) {
+    return v ? gcd(v, u % v) : u;
+}
+void solve() {
+    scanf("%lld%lld%lld", &a, &b, &c);
+    ll ans = a * b / gcd(a, b);
+    ll g = gcd(ans, c);
+    ans = ans * c / g;
+    printf("%lld\n", ans);
+}
+int main() {
+#ifdef MULTI
+    _w = scanf("%d", &_t);
+    while (_t--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+    return 0;
+}
