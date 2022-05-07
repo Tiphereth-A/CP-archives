386f817ab882eba136d9b25ac3d58202a3ea8a62
Tifa <tiphereth-a@qq.com>
2022-05-07 11:12:28 +0800

    feat: add AtCoder jsc2021_c

---
+++ src/AtCoder/jsc2021_c/0.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 5e3 + 5;
+const int MOD = 1e9 + 7;
+int main() {
+#ifndef ONLINE_JUDGE
+    clock_t _CLOCK_ST = clock();
+#endif
+    int a, b;
+    cin >> a >> b;
+    int ans = 1;
+    for (int i = 2, _; i <= b; ++i) {
+        _ = ceil(1.0 * a / i);
+        if (_ * i >= a && _ * i <= b && (_ + 1) * i >= a && (_ + 1) * i <= b) ans = i;
+    }
+    cout << ans;
+#ifndef ONLINE_JUDGE
+    std::cerr
+        << "\n---\n"
+        << "Time used: " << clock() - _CLOCK_ST << std::endl;
+#endif
+    return 0;
+}
