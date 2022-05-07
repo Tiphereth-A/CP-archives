77ae72d20a7eb6cac1ede211513a4d174e31bbe1
Tifa <tiphereth-a@qq.com>
2022-05-07 22:49:19 +0800

    feat: add CodeForces 1437B

---
+++ src/CodeForces/1437B/0.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+#ifndef ONLINE_JUDGE
+    clock_t _CLOCK_ST = clock();
+#endif
+    int kase;
+    cin >> kase;
+    while (kase--) {
+        int n;
+        string str;
+        cin >> n >> str;
+        int cnt = 0, ans = 0;
+        char now = str[0];
+        for (char ch : str) {
+            if (now == ch)
+                ++cnt;
+            else {
+                ans += cnt - 1;
+                cnt = 1;
+                now = ch;
+            }
+        }
+        ans += cnt - 1;
+        cout << (ans + 1) / 2 << endl;
+    __end_kase:;
+    }
+#ifndef ONLINE_JUDGE
+    std::cerr << "\n---\n"
+              << "Time used: " << clock() - _CLOCK_ST << std::endl;
+#endif
+    return 0;
+}
