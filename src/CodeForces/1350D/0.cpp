e8701835d6243aff6dd65c1a3281a600d98ab394
Tifa <tiphereth-a@qq.com>
2022-05-07 10:56:11 +0800

    feat: add CodeForces 1350D

---
+++ src/CodeForces/1350D/0.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+int s[N];
+int main() {
+#ifndef ONLINE_JUDGE
+    clock_t _CLOCK_ST = clock();
+#endif
+    int kase;
+    cin >> kase;
+    while (kase--) {
+        int n, k;
+        cin >> n >> k;
+        bool flag = 0;
+        for (int i = 1; i <= n; ++i) {
+            cin >> s[i];
+            flag |= s[i] == k;
+        }
+        if (!flag) {
+            cout << "no\n";
+            continue;
+        }
+        if (n == 1) {
+            cout << "yes\n";
+            continue;
+        }
+        flag = 0;
+        for (int i = 1; i < n; ++i)
+            if (s[i] >= k && s[i + 1] >= k) {
+                flag = 1;
+                break;
+            }
+        if (!flag)
+            for (int i = 1; i < n - 1; ++i)
+                if (s[i] >= k && s[i + 2] >= k) {
+                    flag = 1;
+                    break;
+                }
+        if (flag)
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+#ifndef ONLINE_JUDGE
+    std::cerr << "\n---\n"
+              << "Time used: " << clock() - _CLOCK_ST << std::endl;
+#endif
+    return 0;
+}
