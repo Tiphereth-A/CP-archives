3de29967f5aaf0eedb172176dfd74c46323fd7f9
Tifa <tiphereth-a@qq.com>
2022-05-07 20:14:33 +0800

    feat: add Nowcoder 236569

--- src/NowCoder/236569/0.cpp
+++ src/NowCoder/236569/0.cpp
@@ -1,19 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 using i64 = int64_t;
+using pii = pair<int, int>;
 #define _for(i, l, r, vals...) for (decltype(l + r) i = (l), i##end = (r), ##vals; i <= i##end; ++i)
 template <class T>
 bool chkmin(T &a, T b) { return b < a ? a = b, true : false; }
 template <class T>
 bool chkmax(T &a, T b) { return a < b ? a = b, true : false; }
 const uint32_t OFFSET = 5;
-const uint32_t N = 1e7 + OFFSET;
+const uint32_t N = 1e6, M = 2e5 + OFFSET, K = 21;
 const uint32_t MOD = 1e9 + 7;
+const double EPS = 1e-6;
+const int INF = 0x3f3f3f3f;
+const i64 INFLL = 0x3f3f3f3f3f3f3f3f;
+const double PI = acos(-1.0);
+const pii DIR4[4] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+const pii DIR8[8] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+const int EXP10[10] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+double harmony[N + OFFSET];
+const double egamma = 0.5772156649015329;
+auto __STATIC__ = []() {
+    harmony[1] = 1;
+    _for(i, 2, N) harmony[i] = harmony[i - 1] + 1.0 / i;
+    return 0.0;
+}();
+#define MULTI_CASES
 inline auto solve() -> void {
-    i64 n;
+    int n;
     cin >> n;
-    cout << (int)sqrt(n);
-    _for(i, 1, (i64)sqrt(n)) cout << ' ' << i * i;
+    double ans = 0;
+    if (n > N)
+        ans = log(n);
+    else
+        ans = harmony[n - 1] - egamma;
+    cout << fixed << setprecision(6) << ans << '\n';
 }
 int main() {
 #ifdef _LOCAL_
