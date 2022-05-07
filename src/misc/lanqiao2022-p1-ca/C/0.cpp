c7c0741b1babe9b0968192f35917741b95cd9c2c
Tifa <tiphereth-a@qq.com>
2022-05-07 23:29:29 +0800

    feat: add lq2022ca

---
+++ src/misc/lanqiao2022-p1-ca/C/0.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+const int N = 2e5 + 5;
+
+int a[N];
+long long s[N];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
+    for (int i = 1; i <= n; ++i)
+        s[i] = s[i - 1] + a[i];
+    long long ans = 0;
+    for (int i = 1; i <= n; ++i)
+        ans += a[i] * (s[n] - s[i]);
+    cout << ans;
+}
