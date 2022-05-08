d0df5ccb672619a994a473737fbc2c9a67de3d7f
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2022-05-08 16:48:42 +0000

    style: reformat

--- src/Luogu/1517/0.java
+++ src/Luogu/1517/0.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         BigDecimal a;
-        while(read.hasNext()) {
+        while (read.hasNext()) {
             a = read.nextBigDecimal();
             int b = read.nextInt();
             String S = a.pow(b).toPlainString();
             int l = 0, ed = S.length();
-            for(int i = 0; i < S.length(); ++i) {
+            for (int i = 0; i < S.length(); ++i) {
                 l = i;
-                if(S.charAt(i) >= '1' && S.charAt(i) <= '9' || S.charAt(i) == '.')break;
+                if (S.charAt(i) >= '1' && S.charAt(i) <= '9' || S.charAt(i) == '.') break;
             }
-            for(int i = S.length() - 1; i >= 0; --i) {
+            for (int i = S.length() - 1; i >= 0; --i) {
                 ed = i;
-                if(S.charAt(i) >= '1' && S.charAt(i) <= '9') break;
-                if(S.charAt(i) == '.') {
+                if (S.charAt(i) >= '1' && S.charAt(i) <= '9') break;
+                if (S.charAt(i) == '.') {
                     --ed;
                     break;
                 }
