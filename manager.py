f23fa09022da91273b6b83c6c736ed13bad644b3
Tifa <tiphereth-a@qq.com>
2022-05-18 18:05:38 +0800

    style: change log color

--- manager.py
+++ manager.py
@@ -14,7 +14,24 @@ from tools.cmcleaner.src.comments_cleaner import cpp, python
 @click.option('-l', '--level', type=click.Choice(['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']), help='log level',
               default='INFO')
 def cli(level: str):
-    coloredlogs.install(level=level, fmt='%(asctime)s <%(name)s:%(process)d> %(levelname)s %(message)s')
+    coloredlogs.install(level=level,
+                        fmt='%(asctime)s %(levelname)s %(programname)s::%(name)s [%(process)d] %(message)s',
+                        field_styles={'asctime': {'color': 'green'},
+                                      'hostname': {'color': 'red'},
+                                      'levelname': {'bold': True, 'color': 'magenta'},
+                                      'name': {'faint': True, 'color': 'blue'},
+                                      'programname': {'bold': True, 'color': 'cyan'},
+                                      'process': {'faint': True, 'color': 'green'},
+                                      'username': {'color': 'yellow'}},
+                        level_styles={'critical': {'bold': True, 'color': 'red'},
+                                      'debug': {'color': 'cyan'},
+                                      'error': {'color': 'red'},
+                                      'info': {'bright': True, 'color': 'white'},
+                                      'notice': {'color': 'magenta'},
+                                      'spam': {'color': 'green', 'faint': True},
+                                      'success': {'bold': True, 'color': 'green'},
+                                      'verbose': {'color': 'blue'},
+                                      'warning': {'bright': True, 'color': 'yellow'}})
 
 
 @cli.command('rne')
