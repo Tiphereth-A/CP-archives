16c5509ab9d98e82e21afcf0200c19c9a22deacb
Tifa <tiphereth-a@qq.com>
2022-05-18 18:07:19 +0800

    feat: add CodeForces 1679B

---
+++ src/CodeForces/1679B/0.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = int64_t;
+#define _for(i, l, r, vals...) for (decltype(l + r) i = (l), i##end = (r), ##vals; i <= i##end; ++i)
+const uint32_t OFFSET = 5;
+const uint32_t N = 2e5 + OFFSET, M = 2e5 + OFFSET, K = 21;
+const uint32_t MOD = 1e9 + 7;
+const double EPS = 1e-6;
+constexpr auto __STATIC__ = []() { return 0.0; }();
+struct Query {
+    int l, r;
+} q[M];
+map<int, int> mp;
+inline auto solve() -> void {
+    int n, m;
+    cin >> n >> m;
+    i64 ans = 0;
+    int global_a = 0;
+    _for(i, 1, n, x) {
+        cin >> x;
+        mp[i] = x;
+        ans += x;
+    }
+    _for(i, 1, m, t, x, y) {
+        cin >> t >> x;
+        if (t == 2) {
+            global_a = x;
+            ans = 1ll * n * x;
+            mp.clear();
+        } else {
+            cin >> y;
+            if (mp[x]) {
+                ans += y - mp[x];
+            } else {
+                ans += y - global_a;
+            }
+            mp[x] = y;
+        }
+        cout << ans << '\n';
+    }
+}
+int main() {
+#ifdef _LOCAL_
+    auto _CLOCK_ST = std::chrono::steady_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifdef MULTI_CASES
+    int _t;
+    cin >> _t;
+    while (_t--)
+#endif
+        solve();
+#ifdef _LOCAL_
+    std::clog << "\n---\n"
+              << "Time used: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - _CLOCK_ST).count() << " ms" << std::endl;
+#endif
+    return 0;
+}
