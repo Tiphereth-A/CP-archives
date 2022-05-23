61334b24c5d42b673242d8e03ded4e3be1233a7d
Tifa <tiphereth-a@qq.com>
2022-05-23 15:16:47 +0800

    feat: add CodeForces 1682B

---
+++ src/CodeForces/1682B/0.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _for(i, l, r, vals...) for (decltype(l + r) i = (l), i##end = (r), ##vals; i <= i##end; ++i)
+template <class T>
+bool chkmin(T &a, T b) { return b < a ? a = b, true : false; }
+template <class T>
+bool chkmax(T &a, T b) { return a < b ? a = b, true : false; }
+const uint32_t OFFSET = 5;
+const uint32_t N = 2e5 + OFFSET, M = 2e5 + OFFSET, K = 21;
+int a[N];
+#define MULTI_CASES
+inline auto solve() -> void {
+    int n;
+    cin >> n;
+    _for(i, 0, n - 1) cin >> a[i];
+    int ans = -1;
+    _for(i, 0, n - 1)
+        if (a[i] != i) ans &= i;
+    cout << ans << '\n';
+}
+int main() {
+#ifdef _LOCAL_
+    auto _CLOCK_ST = std::chrono::steady_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifdef MULTI_CASES
+    int _t;
+    cin >> _t;
+    while (_t--)
+#endif
+        solve();
+#ifdef _LOCAL_
+    std::clog << "\n---\n"
+              << "Time used: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - _CLOCK_ST).count() << " ms" << std::endl;
+#endif
+    return 0;
+}
